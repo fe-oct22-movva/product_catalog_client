863c53c776dfca8393dcc833c03f0f1834ca23c4
Viktoriia Kravchenko <kravchenkovictoriya9@gmail.com>
2023-02-28 02:00:42 +0200

    Added responsive headed

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Header } from './components/HomePage/Header';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Header />
+    </div>
   );
 }
 
---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,8 @@
+import { React } from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="page__body">
+    </div>
+  );
+};
