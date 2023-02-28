153a3c0eb35912fda7e3c5d4f4509805282737b8
Viktoriia Kravchenko <kravchenkovictoriya9@gmail.com>
2023-02-28 18:48:24 +0200

    Created footer branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Header } from './components/HomePage/Header';
-import { Banner } from './components';
-import { Cards } from './ProductCard';
-import { ShopByCategory } from './components/ShopByCategory'
+import { Banner } from './components/Banner';
+import { Cards } from './components/ProductCard';
+import { ShopByCategory } from './components/ShopByCategory';
 import { HomePage } from './components/HomePage/HomePage';
 
 import './styles/main.scss';
