5583bafaac2a8620d908b7e93d61aeaec055ac41
Vladimir Arkanov <vladimirarkanov@icloud.com>
2023-02-28 17:44:27 +0200

    block about phone was done and optimized

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
-// import { Header } from './components/HomePage/Header';
-// import { Banner } from './components';
-// import { Cards } from './ProductCard';
-// import { ShopByCategory } from './components/ShopByCategory';
-// import { HomePage } from './components/HomePage/HomePage';
-import { AboutPhone } from './components/AboutPhone';
+import { Header } from './components/HomePage/Header';
+import { Banner } from './components';
+import { Cards } from './ProductCard';
+import { ShopByCategory } from './components/ShopByCategory';
+import { HomePage } from './components/HomePage/HomePage';
+import {AboutPhone} from './components/AboutPhone';
 
 import './styles/main.scss';
 
 function App() {
   return (
     <div className="App">
-      {/* <Header />
+      <Header />
       <Banner />
       <Cards />
       <ShopByCategory />
-      <HomePage /> */}
+      <HomePage />
       <AboutPhone />
     </div>
   );
--- src/components/AboutPhone/AboutPhone.tsx
+++ src/components/AboutPhone/AboutPhone.tsx
@@ -4,7 +4,6 @@ import './AboutPhone.scss';
 export const AboutPhone: React.FC = () => {
   return (
     <div className="about">
-
       <div className="grid-shop-by grid-shop-by--desktop">
         <div className="grid-shop-by__item grid-shop-by__item--desktop-1-10">
           <h2 className="about__header">About</h2>
@@ -15,31 +14,44 @@ export const AboutPhone: React.FC = () => {
           <section>
             <div className="about__text">
               <p className="about__text-service">
-                A transformative triple‑camera system that adds tons of capability without complexity.
+                A transformative triple‑camera system that adds tons of
+                capability without complexity.
               </p>
 
               <p>
-                An unprecedented leap in battery life. And a mind‑blowing chip that doubles down on machine
-                learning and pushes the boundaries of what a smartphone can do. Welcome to the first
-                iPhone powerful enough to be called Pro.
+                An unprecedented leap in battery life. And a mind‑blowing chip
+                that doubles down on machine learning and pushes the boundaries
+                of what a smartphone can do. Welcome to the first iPhone
+                powerful enough to be called Pro.
               </p>
             </div>
           </section>
 
           <section>
             <h3 className="about__title">Camera</h3>
-            <div className="about__text">Meet the first triple‑camera system to combine cutting‑edge technology with
-              the legendary simplicity of iPhone. Capture up to four times more scene. Get
-              beautiful images in drastically lower light. Shoot the highest‑quality video in a
-              smartphone — then edit with the same tools you love for photos. You&apos;ve never shot with anything like it.</div>
+            <div className="about__text">
+              Meet the first triple‑camera system to combine cutting‑edge
+              technology with the legendary simplicity of iPhone. Capture up to
+              four times more scene. Get beautiful images in drastically lower
+              light. Shoot the highest‑quality video in a smartphone — then edit
+              with the same tools you love for photos. You&apos;ve never shot
+              with anything like it.
+            </div>
           </section>
 
           <section>
-            <h3 className="about__title">Shoot it. Flip it. Zoom it. Crop it. Cut it. Light it. Tweak it. Love it.</h3>
-            <div className="about__text">iPhone 11 Pro lets you capture videos that are beautifully true to life, with
-              greater detail and smoother motion. Epic processing power means it can shoot 4K
-              video with extended dynamic range and cinematic video stabilization — all at 60
-              fps. You get more creative control, too, with four times more scene and powerful new editing tools to play with.</div>
+            <h3 className="about__title">
+              Shoot it. Flip it. Zoom it. Crop it. Cut it. Light it. Tweak it.
+              Love it.
+            </h3>
+            <div className="about__text">
+              iPhone 11 Pro lets you capture videos that are beautifully true to
+              life, with greater detail and smoother motion. Epic processing
+              power means it can shoot 4K video with extended dynamic range and
+              cinematic video stabilization — all at 60 fps. You get more
+              creative control, too, with four times more scene and powerful new
+              editing tools to play with.
+            </div>
           </section>
         </div>
 
