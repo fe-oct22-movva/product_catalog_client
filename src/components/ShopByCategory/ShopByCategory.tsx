130fe2ed056625ade86700c51620794132711495
Olena Yanovska <alena.yn.adw@gmail.com>
2023-03-01 14:34:38 +0200

    deleted old grid file

--- src/components/ShopByCategory/ShopByCategory.tsx
+++ src/components/ShopByCategory/ShopByCategory.tsx
@@ -10,14 +10,13 @@ export const ShopByCategory: React.FC = () => {
       <div className="container">
         <h2 className="shopByCategory__title">Shop by category</h2>
 
-        <div className="grid-shop-by grid-shop-by--tablet grid-shop-by--desktop">
+        <div className="grid grid--tablet grid--desktop">
           <div
             className="
               shopByCategory__item
-              grid-shop-by__item
-              grid-shop-by__item--tablet-1-4
-              grid-shop-by__item--desktop-1-8"
-          >
+              grid__item
+              grid__item--tablet-1-4
+              grid__item--desktop-1-8">
             <CategoryCard
               imageUrl={Phones}
               imageAlt="Mobile phones category"
@@ -30,10 +29,9 @@ export const ShopByCategory: React.FC = () => {
           <div
             className="
               shopByCategory__item
-              grid-shop-by__item
-              grid-shop-by__item--tablet-5-8
-              grid-shop-by__item--desktop-9-16"
-          >
+              grid__item
+              grid__item--tablet-5-8
+              grid__item--desktop-9-16">
             <CategoryCard
               imageUrl={Tablets}
               imageAlt="Tablets category"
@@ -46,10 +44,9 @@ export const ShopByCategory: React.FC = () => {
           <div
             className="
               shopByCategory__item
-              grid-shop-by__item
-              grid-shop-by__item--tablet-9-12
-              grid-shop-by__item--desktop-17-24"
-          >
+              grid__item
+              grid__item--tablet-9-12
+              grid__item--desktop-17-24">
             <CategoryCard
               imageUrl={Accessoires}
               imageAlt="Accessories category"
