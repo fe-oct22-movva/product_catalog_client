eada8857a87318b02b682ea573e05ad7f864b176
Viktoriia Kravchenko <kravchenkovictoriya9@gmail.com>
2023-03-01 15:25:36 +0200

    Finished header

---
+++ src/components/HomePage/Header/HeaderIconNavLink.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from 'react';
+import { NavLink, To } from 'react-router-dom';
+import cn from 'classnames';
+
+type Props = {
+  to: To;
+  textToDisplay: string | ReactNode;
+};
+
+export const HeaderIconNavLink: React.FC<Props> = ({ to, textToDisplay }) => (
+  <NavLink
+    to={to}
+    className={({ isActive }) => cn('nav__link nav__link--icon', { 'nav__link--activeIcon': isActive })}
+  >
+    {textToDisplay}
+  </NavLink>
+);
