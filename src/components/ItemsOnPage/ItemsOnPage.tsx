8f8f5d3bb39cb8e280be8b4a7743d27467ab8182
Olena Yanovska <alena.yn.adw@gmail.com>
2023-03-03 02:01:30 +0200

    added mobile markup in grid

---
+++ src/components/ItemsOnPage/ItemsOnPage.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import styles from '../ItemsOnPage/ItemsOnPage.module.scss';
+import arrowDown from '../../assets/images/ArrowDown.svg';
+
+const options2 = [16, 20, 24];
+
+export const ItemsOnPage: React.FC = () => {
+  const [isItemsOnPageOpen, setIsItemsOnPageOpen] = useState(false);
+
+  const handleItemsOnPage = () => {
+    setIsItemsOnPageOpen(!isItemsOnPageOpen);
+  };
+
+  return (
+    <div className={`
+      ${styles.sortBy}
+      grid__item--mobile-3-4
+      grid__item--tablet-5-7 
+      grid__item--desctop-57
+    `}>
+      <p className={styles.sortBy__description}>Items on page</p>
+
+      <div className={styles.dropdown}>
+        <button className={styles.dropdown__header}
+          onClick={handleItemsOnPage}
+        >
+          <div className={styles.dropdown__title}>
+            {options2[0]}
+            <img
+              className={styles.dropdown__arrow}
+              src={arrowDown}
+            />
+          </div>
+        </button>
+
+        {isItemsOnPageOpen === false ? (
+          <p></p>
+        ) : (
+          <div className={styles.dropdown__items}>
+            {options2.map((option) => (
+              <button className={styles.dropdown__option}>
+                {option}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
---
+++ src/components/SortBy/SortBy.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import styles from '../SortBy/SortBy.module.scss';
+import arrowDown from '../../assets/images/ArrowDown.svg';
+
+const options1 = ['Newest', 'Lowest price', 'Highest price']
+
+export const SortBy: React.FC = () => {
+  const [isSortByOpen, setIsSortByOpen] = useState(false);
+
+  const handleSortBy = () => {
+    setIsSortByOpen(!isSortByOpen);
+  };
+
+  return (
+    <div className={`
+      ${styles.sortBy}
+      grid__item--mobile-1-2
+      grid__item--tablet-1-4 
+      grid__item--desctop-1-4
+    `}>
+      <div>
+        <p className={styles.sortBy__description}>
+          Sort by
+        </p>
+      </div>
+
+      <div className={styles.dropdown}>
+        <button className={styles.dropdown__header}
+          onClick={handleSortBy}>
+          <div className={styles.dropdown__title}>
+            {options1[0]}
+          </div>
+          <img
+            className={styles.dropdown__arrow}
+            src={arrowDown}
+          />
+        </button>
+
+        {isSortByOpen === false ? (
+          <p></p>
+        ) : (
+          <div className={styles.dropdown__items}>
+            {options1.map((option) => (
+              <button className={styles.dropdown__option}>
+                {option}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+
+  );
+};
