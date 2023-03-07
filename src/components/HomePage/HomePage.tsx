40473ff0222d9a511019c2b934a9cab760e7c16d
Viktoriia Kravchenko <kravchenkovictoriya9@gmail.com>
2023-03-07 20:12:08 +0200

    Added loader to the Home page

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import {useEffect, useState} from 'react';
 import {getAllPhones} from '../../api/phones';
 import {Phone} from '../../types/types';
 import {Banner} from '../Banner';
+import { Loader } from '../Loader';
 import {Cards} from '../ProductCard';
 import {ShopByCategory} from '../ShopByCategory';
 
@@ -14,8 +16,11 @@ export const HomePage: React.FC<Props> = ({setSelectedId}) => {
   const [newestPhones, setNewestPhones] = useState<Phone[]>([]);
   const [cheapestPhones, setCheapestPhones] = useState<Phone[]>([]);
   const [phonesNumber, setPhonesNumber] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    setTimeout(() => setIsLoading(false), 2000);
+
     getAllPhones([
       ['sort', 'newest'],
       ['limit', '12'],
@@ -25,7 +30,10 @@ export const HomePage: React.FC<Props> = ({setSelectedId}) => {
         setPhonesNumber(data.totalPhones);
         console.log(data);
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        console.log(error);
+        Notify.failure('Oops, something went wrong. Please try again later.');
+      });
 
     getAllPhones([
       ['sort', 'cheapest'],
@@ -35,7 +43,10 @@ export const HomePage: React.FC<Props> = ({setSelectedId}) => {
         setCheapestPhones(data.result);
         console.log(data);
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        console.log(error);
+        Notify.failure('Oops, something went wrong. Please try again later.');
+      });
   }, []);
 
   return (
@@ -43,22 +54,27 @@ export const HomePage: React.FC<Props> = ({setSelectedId}) => {
       <head>
         <title>Nice Gadgets store</title>
       </head>
-      <div>
-        <Banner />
-        <div className="main-container">
-          <Cards
-            newestPhones={newestPhones}
-            setSelectedId={setSelectedId}
-            title="Brand new models"
-          />
-          <ShopByCategory phonesNumber={phonesNumber} />
-          <Cards
-            newestPhones={cheapestPhones}
-            setSelectedId={setSelectedId}
-            title="Hot prices"
-          />
+
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <Banner />
+          <div className="main-container">
+            <Cards
+              newestPhones={newestPhones}
+              setSelectedId={setSelectedId}
+              title="Brand new models"
+            />
+            <ShopByCategory phonesNumber={phonesNumber} />
+            <Cards
+              newestPhones={cheapestPhones}
+              setSelectedId={setSelectedId}
+              title="Hot prices"
+            />
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
