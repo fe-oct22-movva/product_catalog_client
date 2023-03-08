f53cdbf133711095d963d50d8333029d364bcdf4
Olena Yanovska <alena.yn.adw@gmail.com>
2023-03-08 18:12:00 +0200

    created personal cards

---
+++ src/components/PersonCard/PersonCard.tsx
@@ -0,0 +1,58 @@
+import styles from './PersonCard.module.scss';
+
+interface Props {
+  photo: string;
+  name: string;
+  linkedinUrl: string;
+  githubUrl: string;
+}
+
+export const PersonCard: React.FC<Props> = ({
+  photo,
+  name,
+  linkedinUrl,
+  githubUrl,
+}) => {
+  return (
+    <div className={styles.person}>
+      <img className={styles.person__photo} src='' alt='person photo' />
+      <h3 className={styles.person__name}>
+        {name}
+      </h3>
+      <div className={styles.person__description}>
+        <a
+          href={linkedinUrl}
+          target="_blank"
+          rel="noreferrer"
+          className={styles.person__link}
+        >
+          Linkedin
+        </a>
+        <a
+          href=""
+          target="_blank"
+          rel="noreferrer"
+          className={styles.person__link}
+        >
+          Dou
+        </a>
+        <a
+          href={githubUrl}
+          target="_blank"
+          rel="noreferrer"
+          className={styles.person__link}
+        >
+          Github
+        </a>
+        <a
+          href=""
+          target="_blank"
+          rel="noreferrer"
+          className={styles.person__link}
+        >
+          Djinni
+        </a>
+      </div>
+    </div>
+  )
+}
--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import styles from '../Contacts/Contacts.module.scss';
-import {Breadcrumbs} from '../../components/Breadcrumbs';
+import styles from './Contacts.module.scss';
+import { Breadcrumbs } from '../../components/Breadcrumbs';
+import { PersonCard } from '../../components/PersonCard/PersonCard';
 
 export const Contacts: React.FC = () => {
   return (
@@ -8,12 +9,58 @@ export const Contacts: React.FC = () => {
       <head>
         <title>Contacts</title>
       </head>
+
       <div className="main-container">
         <Breadcrumbs />
-        <section className={styles.contacts}>
-          <h1 className={styles.contacts__title}>Contacts</h1>
-          <p>Page is waiting for content</p>
-        </section>
+        <div className='grid grid--mobile grid--tablet grid--desktop'>
+        <div className={styles.contacts}>
+          <h1 className={styles.contacts__title}>
+            Contacts
+          </h1>
+          <div className={styles.contacts__description}>
+            <p className={styles.contacts__description__text}>
+              We are creative and motivated Full-Stack Developers, who created this online store. You can contact with any of us using the links below.
+            </p>
+          </div>
+
+          <div className={styles.cards}>
+            <PersonCard
+              photo=''
+              name='Artem Hrechuk'
+              linkedinUrl='https://www.linkedin.com/in/artem-hrechuk-9b0222265/'
+              githubUrl='https://github.com/official-artem'
+            />
+
+            <PersonCard
+              photo=''
+              name='Maksim Levchenko'
+              linkedinUrl='https://www.linkedin.com/in/maksim-levchenko-45238b265/'
+              githubUrl='https://github.com/llllewvvaa'
+            />
+
+            <PersonCard
+              photo=''
+              name='Olena Yanovska'
+              linkedinUrl='https://www.linkedin.com/in/olena-yanovska-69a31b265/'
+              githubUrl='https://github.com/olena-yanovska'
+            />
+
+            <PersonCard
+              photo=''
+              name='Vika Kravchenko'
+              linkedinUrl='https://www.linkedin.com/in/viktoriia-kravchenko-5110b1265/'
+              githubUrl='https://github.com/viktoriia-kravchenko'
+            />
+
+            <PersonCard
+              photo=''
+              name='Volodymyr Arkanov'
+              linkedinUrl='https://www.linkedin.com/in/volodymyr-arkanov-b1a116265/'
+              githubUrl='https://github.com/vladimir-arkanov'
+            />
+          </div>
+        </div>
+      </div>
       </div>
     </>
   );
